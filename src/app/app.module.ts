fd734709efc8c9aa6c7d8e34c682656ecdef0cff
Gabriel-Karajallo <c.gabrielkarajallo@gmail.com>
2024-10-28 12:38:12 +0100

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { SharedModule } from '../../../heroesApp/src/app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LoginComponent } from './auth/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   exports:[
     LoginComponent
